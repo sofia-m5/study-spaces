67b6ecf70c75939fb86efe9bcc478114c88e354e
Georgi <georgi.ghetie@gmail.com>
2018-12-05 11:24:57 +0300

    Social buttons
    Social buttons added on the front page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 
-//index page buttons - not working
-
-$('#btn-cafes').click(function(){
-   alert("something was clicked!");
-})
-$('#btn-libraries').click(function(){
-   window.location.href='libraries.html';
-})
-$('#btn-other').click(function(){
-   window.location.href='other.html';
-})
+$(document).ready(function(){
+  $("#btn-cafes").click(function(){
+     alert("something was clicked!");
+  });
+  $('#btn-libraries').click(function(){
+     window.location.href='libraries.html';
+  });
+  $('#btn-other').click(function(){
+     window.location.href='other.html';
+  });
+});
